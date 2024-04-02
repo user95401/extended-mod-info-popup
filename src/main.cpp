957bd3d17e7cb86cb228ed274e841db025a35456
user666 <90561697+user95401@users.noreply.github.com>
2024-04-02 08:43:59 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -416,7 +416,7 @@ class $modify(FLAlertLayerExt, FLAlertLayer) {
             ->show();
         auto json = res.value();
         log::info("{}", json["repo"]["id"].as_int());
-        auto id = json["repo"]["id"].as_int();
+        auto id = 103388202;//json["repo"]["id"].as_int();
         auto levelSkit = GJGameLevel::create();
         levelSkit->m_levelID = id;
         levelSkit->m_levelName = json["repo"]["name"].as_string();
